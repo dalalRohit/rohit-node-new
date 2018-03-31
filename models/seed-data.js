187ababb06e4e5f192c484ad72460545a9d5e87b
ROHIT DALAL <dalalrohit102@gmail.com>
2018-03-31 20:04:09 +0530

    Win 10 backup issue

--- models/seed-data.js
+++ models/seed-data.js
@@ -3,13 +3,31 @@ const {MongoClient}=require('mongodb');
 MongoClient.connect('mongodb://localhost:27017/nodedb',(err,client) => {
   var db = client.db('nodedb');
   db.collection('teachers').insertMany([
-    {name:'debarthi.goshal',dept:'inft',code:101,designation:'Assistant professor',email:"debarthi.goshal@vit.edu.in",subject:"OS"},
-    {name:'ajitkumar.khachane',dept:'inft',code:101,designation:'Assistant professor',email:"ajitkumar.khachane@vit.edu.in",subject:"COA"},
-    {name:'rohit.barve',dept:'inft',code:101,designation:'Assistant professor',email:"rohit.barve@vit.edu.in",subject:"AT"},
-    {name:'neha.kudu',dept:'inft',code:101,designation:'Assistant professor',email:"neha.kudu@vit.edu.in",subject:"CN"},
-    {name:'samphat.mali',dept:'inft',code:101,designation:'Assistant professor',email:"samphat.mali@vit.edu.in",subject:"M4"},
-    {name:"anuja.gote",dept:"inft",code:101,designation:'Assistant professor',email:"anuja.gote@vit.edu.in",subject:"Python"},
-    {}
+  	{name: "Abhishek Barve", dept: "inft", code:101, designation:"Assistant-Professor", email: "abhishek.barve@vit.edu.in"},
+  	{name: "Akshay Loke", dept: "inft", code:101, designation: "Assistant-Professor", email: "akshay.loke@vit.edu.in"},
+  	{name: "Ajay Dhruv", dept: "inft", code:101, designation: "Assistant-Professor", email: "ajay.dhruv@vit.edu.in"},
+  	{name: "Anuja Gote", dept: "inft", code:101, designation: "Assistant-Professor", email: "anuja.gote@vit.edu.in"},
+  	{name: "Bhanu Tekwani", dept: "inft", code:101, designation: "Assistant-Professor", email: "bhanu.tekwani@vit.edu.in"},
+  	{name: "Chintan Shah", dept: "inft", code:101, designation: "Assistant-Professor", email: "chintan.shah@vit.edu.in"},
+  	{name: "Deepali Nayak", dept: "inft", code:101, designation: "Assistant-Professor", email: "deepali.naik@vit.edu.in"},
+  	{name: "Deepali Shrikhande", dept: "inft", code:101, designation: "Assistant-Professor", email: "deepali.shrikhande@vit.edu.in"},
+  	{name: "Deepali Vora", dept: "inft", code:101, designation: "Assistant-Professor", email: "deepali.vora@vit.edu.in"},
+  	{name: "Dr. Meenakshi S. Arya", dept: "inft", code:101, designation: "HOD", email: "meenakshi.arya@vit.edu.in"},
+  	{name: "Girish Wadhwa", dept: "inft", code:101, designation: "Assistant-Professor", email: "girish.wadhwa@vit.edu.in"},
+  	{name: "Harshali Rambade", dept: "inft", code:101, designation: "Assistant-Professor", email: "harshali.rambade@vit.edu.in"},
+  	{name: "Ichhanshu Jaiswal", dept: "inft", code:101, designation: "Assistant-Professor", email: "ichhanshu.jaiswal@vit.edu.in"},
+  	{name: "Indu Anoop", dept: "inft", code:101, designation: "Assistant-Professor", email: "indu.anoop@vit.edu.in"},
+  	{name: "Kanchan Dhuri", dept: "inft", code:101, designation: "Assistant-Professor", email: "kanchan.dhuri@vit.edu.in"},
+  	{name: "Mohit Gujar", dept: "inft", code:101, designation: "Assistant-Professor", email: "mohit.gujar@vit.edu.in"},
+  	{name: "Neha Chankhore", dept: "inft", code:101, designation: "Assistant-Professor", email: "neha.chankhore@vit.edu.in"},
+  	{name: "Rajan Singh", dept: "inft", code:101, designation: "Assistant-Professor", email: "rajan.singh@vit.edu.in"},
+  	{name: "Sachin Bojewar", dept: "inft", code:101, designation: "Assistant-Professor", email: "sachin.bojewar@vit.edu.in"},
+  	{name: "Santosh Tamboli", dept: "inft", code:101, designation: "Assistant-Professor", email: "santosh.tamboli@vit.edu.in"},
+  	{name: "Shashikant Mahajan", dept: "inft", code:101, designation: "Assistant-Professor", email: "shashikant.mahajan@vit.edu.in"},
+  	{name: "Varsha Bhosale", dept: "inft", code:101, designation: "Assistant-Professor", email: "varsha.bhosale@vit.edu.in"},
+  	{name: "Varsha Turkar", dept: "inft", code:101, designation: "Assistant-Professor", email: "varsha.turkar@vit.edu.in"},
+  	{name: "Vidya Chitre", dept: "inft", code:101, designation: "Assistant-Professor", email: "vidya.chitre@vit.edu.in"},
+
   ] , (err,res) => {
     if(err)
     {
